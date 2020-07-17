2625170071d5bda07fa4887c2ad1b0fa758bdd3d
AlexisHuayna <huaynalexis@gmail.com>
2020-07-16 22:05:49 -0500

    miss user list

--- frontend/src/app/main/participation/participation-list/participation-list.component.ts
+++ frontend/src/app/main/participation/participation-list/participation-list.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { ParticipationService } from 'src/app/services/participation/participation.service';
+import { Participation } from 'src/app/other/interfaces';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-participation-list',
@@ -7,9 +10,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ParticipationListComponent implements OnInit {
 
-  constructor() { }
+   @Input() props: {userId: string; projectId: string; };
+   public participations: Participation[];
+   constructor(public participationService: ParticipationService) {
+      if (this.props != null) {
+         let participationObservable: Observable<Participation[]>;
+         if (this.props.userId != null) {
+            participationObservable = participationService.getParticipationsUser(this.props.userId);
+         } else {
+            participationObservable = participationService.getParticipationsProject(this.props.projectId);
+         }
+         participationObservable.subscribe(participationsResponse => {
+            this.participations = participationsResponse;
+         });
+      }
+   }
 
-  ngOnInit() {
-  }
+   ngOnInit() {
+   }
 
 }
