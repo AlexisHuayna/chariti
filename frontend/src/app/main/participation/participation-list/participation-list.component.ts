30e4f4a12a35143b631e6a81c20fe94d70a80d28
Alexis <ahuayna@multicoop.com.pe>
2020-07-18 12:53:19 -0500

    participation list fixed

--- frontend/src/app/main/participation/participation-list/participation-list.component.ts
+++ frontend/src/app/main/participation/participation-list/participation-list.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { ParticipationService } from 'src/app/services/participation/participation.service';
-import { Participation } from 'src/app/other/interfaces';
+import { Participation, Project, User } from 'src/app/other/interfaces';
 import { Observable } from 'rxjs';
+import { UserService } from 'src/app/services/user/user.service';
+import { ProjectService } from 'src/app/services/project/project.service';
 
 @Component({
   selector: 'app-participation-list',
@@ -12,21 +14,48 @@ export class ParticipationListComponent implements OnInit {
 
    @Input() props: {userId: string; projectId: string; };
    public participations: Participation[];
-   constructor(public participationService: ParticipationService) {
+   public usersOnParticipation: User[];
+   public projectsOnParticipation: Project[];
+   constructor(public participationService: ParticipationService, public userService: UserService, public projectService: ProjectService) {
+      this.getParticipations();
+   }
+
+   ngOnInit() {
+   }
+
+   public getParticipations(): void {
       if (this.props != null) {
          let participationObservable: Observable<Participation[]>;
          if (this.props.userId != null) {
-            participationObservable = participationService.getParticipationsUser(this.props.userId);
+            participationObservable = this.participationService.getParticipationsUser(this.props.userId);
          } else {
-            participationObservable = participationService.getParticipationsProject(this.props.projectId);
+            participationObservable = this.participationService.getParticipationsProject(this.props.projectId);
          }
          participationObservable.subscribe(participationsResponse => {
             this.participations = participationsResponse;
+            if (this.props.userId != null) {
+               this.setProjects();
+            } else {
+               this.setUsers();
+            }
          });
       }
    }
 
-   ngOnInit() {
+   public setUsers(): void {
+      this.participations.forEach(participation => {
+         this.userService.getUser(participation.UserId).subscribe(user => {
+            this.usersOnParticipation.push(user);
+         });
+      });
+   }
+
+   public setProjects(): void {
+      this.participations.forEach(participation => {
+         this.projectService.getProject(participation.ProjectId).subscribe(project => {
+            this.projectsOnParticipation.push(project);
+         });
+      });
    }
 
 }
